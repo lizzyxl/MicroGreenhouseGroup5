956d50608b76cbc7ae89ae68b6da44edf4dc5f87
Simser-98 <simonecapra2007@gmail.com>
2025-12-11 15:09:10 +0100

    i added temperature and humidity sensor and call it in main.c

--- main/main.c
+++ main/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "temp_hum_sensor.h"
 
 void app_main(void)
 {
 
+    float temperature = 0.0;
+    float humidity = 0.0;
+
+    dht11_init();
+
+    while (1)
+    {
+        dht11_read(&temperature, &humidity);
+    }
 }
---
+++ main/temp_hum_sensor.c
@@ -0,0 +1,26 @@
+#include "temp_hum_sensor.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "dht.h"
+
+void dht11_init()
+{
+    // Enable pull-up resistor on the DHT data pin
+    gpio_set_pull_mode(DHT_GPIO, GPIO_PULLUP_ONLY);
+}
+
+void dht11_read(float *temperature, float *humidity)
+{
+    // Read humidity and temperature from the sensor
+    if (dht_read_float_data(SENSOR_TYPE, DHT_GPIO, humidity, temperature) == ESP_OK)
+    {
+        // Print the sensor values if read was successful
+        printf("Humidity: %.1f%%  Temp: %.1f°C\n", *humidity, *temperature);
+    }
+    else
+    {
+        printf("Could not read data from sensor\n");
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(150)); // Delay for 1 second before next read
+}
