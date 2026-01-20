1534f6ba15e7bdb87069e07ed854cc47333a3937
Lizzy Leummens <L.Leummens-1@student.tudelft.nl>
2026-01-20 13:11:51 +0100

    config changes

--- main/include/config.h
+++ main/include/config.h
@@ -1,13 +1,13 @@
 #define PIN_NUM_SDA               21 // SDA GPIO
 #define PIN_NUM_SCL               22 // SCL GPIO
-#define RED_WIFI_LED              GPIO_NUM_12
+#define RED_WIFI_LED              GPIO_NUM_18
 #define BUTTON_PIN_WHITE          GPIO_NUM_13
-#define BUTTON_PIN_BLUE           GPIO_NUM_32
+#define BUTTON_PIN_BLUE           GPIO_NUM_14
 #define GROW_LIGHT_GPIO           GPIO_NUM_25
 #define FAN_GPIO                  GPIO_NUM_26
 #define DHT_GPIO                  GPIO_NUM_27
 #define PUMP_GPIO                 GPIO_NUM_15
-#define MOISTURE_LED              GPIO_NUM_17
-#define SOIL_ADC_CHANNEL          ADC1_CHANNEL_1   // GPIO37
-#define ADC_CHANNEL               ADC_CHANNEL_9    // GPIO33 for LDR
+#define MOISTURE_LED              GPIO_NUM_19
+#define SOIL_ADC_CHANNEL          ADC1_CHANNEL_4   // GPIO32
+#define ADC_CHANNEL               ADC_CHANNEL_0    // GPIO33 for LDR
 #define ADC_UNIT                  ADC_UNIT_2       // ADC2 for LDR
