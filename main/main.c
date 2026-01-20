54b55fce0bcd3748b90bc7a3cc921ee165cf5f00
mi60sumaz <michael.g.petrov@gmail.com>
2026-01-20 14:50:54 +0100

    Merged commits

--- main/main.c
+++ main/main.c
@@ -11,9 +11,18 @@
 #include "soil_moisture.h"
 #include "outputs.h"
 #include "config.h"
+#include "wifi.h"
+#include "mqtt.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
 
 #define TAG "AUTOMATED_GREENHOUSE"
 #define DISPLAY_INTERVAL_MS 100
+
+// WiFi config
+#define WIFI_SSID "Lackner@mobile"
+#define WIFI_PASS "yvka1961"
 
 // Config
 #define DEFAULT_MEASUREMENT_INTERVAL_MS 5000
@@ -23,7 +32,8 @@
 #define DEFAULT_PUMP_SOILMOIST_HIGHER_THRESHOLD 50.0f
 #define DEFAULT_GROWLIGHT_LIGHT_THRESHOLD 50.0f
 
-typedef struct {
+typedef struct
+{
     uint32_t measurement_interval_ms;
     float fan_temp_higher_threshold_C;
     float fan_temp_lower_threshold_C;
@@ -46,24 +56,41 @@ static greenhouse_config_t greenhouse_config = default_config;
 static uint32_t last_measurement_time = 0;
 static uint32_t last_display_time = 0;
 
-void take_measurement (measurements_t *measurment) {
+void take_measurement(measurements_t *measurment)
+{
     measurment->soil_moisture = soil_sensor_read();
     measurment->light = ldr_read_percent();
     aht20_read(&measurment->temperature, &measurment->relative_humidity);
 
     ESP_LOGI(TAG, "Measurement taken: temperature: %.2f C, relative humidity: %.2f %%, soil moisture: %.2f %%, light intensity: %.2f %%", measurment->temperature, measurment->relative_humidity, measurment->soil_moisture, measurment->light);
-            
 }
 
-void reset_to_default_config(greenhouse_config_t *config) {
+void reset_to_default_config(greenhouse_config_t *config)
+{
     memcpy(config, &default_config, sizeof(greenhouse_config_t));
 }
 
-
 void app_main(void)
 {   
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+    // WiFi init
+    ESP_LOGI(TAG, "Connecting to WiFi...");
+    ESP_ERROR_CHECK(wifi_init_sta(WIFI_SSID, WIFI_PASS));
+    ESP_LOGI(TAG, "WiFi connected");
+
+    // MQTT init
+    ESP_LOGI(TAG, "Starting MQTT...");
+    ESP_ERROR_CHECK(mqtt_start());
+
     greenhouse_display_init();
-    // Initialize components
+    //  Initialize components
     fan_init();
     pump_init();
     ldr_init();
@@ -83,14 +110,16 @@ void app_main(void)
     };
     led_state current_moisture_led_state = LED_OFF;
 
-    while(1) {
+    while (1)
+    {
         uint32_t now = esp_log_timestamp();
-       
-        if (now - last_measurement_time >= greenhouse_config.measurement_interval_ms) {
+
+        if (now - last_measurement_time >= greenhouse_config.measurement_interval_ms)
+        {
 
             // sensor measurements
             take_measurement(&current_measurements);
-            
+
             // Publish measurements (TODO)
 
             // actuators
@@ -102,10 +131,17 @@ void app_main(void)
         }
 
         // outputs
-        if (now - last_display_time >= DISPLAY_INTERVAL_MS) {
+        if (now - last_display_time >= DISPLAY_INTERVAL_MS)
+        {
             // button input
+            if (white_button_pressed)
+            {
+                ESP_LOGI(TAG, "Switching display");
+                white_button_pressed = false;
+            }
 
-            if (blue_button_pressed) {
+            if (blue_button_pressed)
+            {
                 ESP_LOGI(TAG, "Taking extra measurement");
                 take_measurement(&current_measurements);
                 blue_button_pressed = false;
@@ -120,8 +156,31 @@ void app_main(void)
             
             last_display_time = now;
         }
-        
-        vTaskDelay(pdMS_TO_TICKS(10));
+        if (mqtt_is_connected())
+        {
+            char buf[16];
+
+            // publish moisture percentage
+            snprintf(buf, sizeof(buf), "%.2f", current_measurements.temperature);
+            mqtt_publish("greenhouse/temperature", buf, 1, 0);
+
+            // Relative humidity
+            snprintf(buf, sizeof(buf), "%.2f", current_measurements.relative_humidity);
+            mqtt_publish("greenhouse/humidity", buf, 1, 0);
+
+            // Soil moisture
+            snprintf(buf, sizeof(buf), "%.2f", current_measurements.soil_moisture);
+            mqtt_publish("greenhouse/soil", buf, 1, 0);
+
+            // Light intensity
+            snprintf(buf, sizeof(buf), "%.2f", current_measurements.light);
+            mqtt_publish("greenhouse/light", buf, 1, 0);
+        }
+        else
+        {
+            ESP_LOGW(TAG, "MQTT not connected, skipping publish");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-
