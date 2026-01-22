31b19e9fdc236216df6ef880958f5aa6959feda9
lackn <lackner.moritz@gmail.com>
2026-01-22 22:35:12 +0100

    added description of key-value pairs

--- main/main.c
+++ main/main.c
@@ -1,3 +1,22 @@
+//API key value pairs for MQTT/UART
+// "measurement_interval_s":int_value //sets time in seconds between each measurement cycle
+// "fan_temp_lower_threshold_C":float_value //set threshold when the fan should turn off again in C
+// "fan_temp_higher_threshold_C":float_value //set threshold when the fan should turn on in C
+// "fan_hum_lower_threshold_pct":float_value //set threshold when the fan should turn off again in %
+// "fan_hum_higher_threshold_pct":float_value //set threshold when the fan should turn on in %
+// "pump_soilmoist_threshold_pct":float_value //set threshold when the pump should turn on in %
+// "growlight_light_threshold_pct":float_value //set threshold when the growlight should turn on %
+// "fan_override":bool_value //set the fan to manual mode (true) or automatic (false)
+// "fan":bool_value //set the fan to manual mode and to on or off
+// "pump_override":bool_value //set the pump to manual mode (true) or automatic (false)
+// "pump":bool_value //set the pump to manual mode and to on or off
+// "growlight_override":bool_value //set the grow light to manual mode (true) or automatic (false)
+// "growlight":bool_value //set the growlight to manual mode and to on or off
+// "config":"default" // resets whole config to default values
+// "wifi_ssid":char_string //sets wifi name
+// "wifi_password":char_string // sets wifi password
+// if one of these values is updated a measurement is executed imidiately
+
 #include <stdio.h>
 #include "esp_log.h"
 #include "fan.h"
