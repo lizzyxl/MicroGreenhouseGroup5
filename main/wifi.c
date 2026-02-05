5fb2e0beea4788feb78c792f6468c5699ca6dc87
lackn <lackner.moritz@gmail.com>
2026-02-05 17:22:31 +0100

    added more visibility to ip to see more easily in the log

--- main/wifi.c
+++ main/wifi.c
@@ -43,10 +43,13 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t e
     }
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
     {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         s_retry_num = 0;
         s_connected = true;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        ESP_LOGI(TAG, "Got IP");
+        ESP_LOGI(TAG, "==================================");
+        ESP_LOGI(TAG, "Connected! IP: " IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(TAG, "==================================");
     }
 }
 
